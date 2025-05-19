19fb5a1014e99fd3b244ff68383bac811f97951e
Nguyen Duc Anh <nguyenducanh2105@gmail.com>
2025-05-19 10:36:24 +0900

    add cursor pointer on date range tabs

--- src/components/DateRangeTabs.jsx
+++ src/components/DateRangeTabs.jsx
@@ -6,28 +6,28 @@ const DateRangeTabs = (props) => {
   return (
     <div className='w-full h-10 mb-4 grid grid-cols-4 gap-2 text-sm'>
       <div
-        className={`w-full h-full flex justify-center items-center rounded-md 
+        className={`w-full h-full flex justify-center items-center rounded-md cursor-pointer 
           ${dateRange == 0 ? 'bg-primary text-white' : 'text-gray-text'}`}
         onClick={() => setDateRange(0)}
       >
         Ngày
       </div>
       <div
-        className={`w-full h-full flex justify-center items-center rounded-md 
+        className={`w-full h-full flex justify-center items-center rounded-md cursor-pointer 
           ${dateRange == 1 ? 'bg-primary text-white' : 'text-gray-text'}`}
         onClick={() => setDateRange(1)}
       >
         Tuần
       </div>
       <div
-        className={`w-full h-full flex justify-center items-center rounded-md 
+        className={`w-full h-full flex justify-center items-center rounded-md cursor-pointer 
           ${dateRange == 2 ? 'bg-primary text-white' : 'text-gray-text'}`}
         onClick={() => setDateRange(2)}
       >
         Tháng
       </div>
       <div
-        className={`w-full h-full flex justify-center items-center rounded-md 
+        className={`w-full h-full flex justify-center items-center rounded-md cursor-pointer 
           ${dateRange == 3 ? 'bg-primary text-white' : 'text-gray-text'}`}
         onClick={() => setDateRange(3)}
       >
